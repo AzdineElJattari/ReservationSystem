1f933bbf924fcf4c826f310177ffe2f0bd3e8c08
AzdineElJattari <azdine.eljattari@hotmail.com>
2021-04-14 03:08:56 +0200

    Fixed bugs inside Home page, Added routing to different pages, Added Shoppingcart interface, Added simple styling to Shop page

--- WebshopBouidi/App_Start/RouteConfig.cs
+++ WebshopBouidi/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace WebshopBouidi
@@ -16,7 +12,22 @@ namespace WebshopBouidi
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Home", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Shop",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Shop", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Appointment",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Appointment", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Contact",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Contact", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ WebshopBouidi/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace WebshopBouidi.Controllers
+{
+    public class ShopController : Controller
+    {
+        public ActionResult Index() //Naam moet overeenkomen met de naam van de view
+        {
+            return View();
+        }
+    }
+}
