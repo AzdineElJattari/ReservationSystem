3d4e1771d0abd133e03c493bb77508cc48b8864b
AzdineElJattari <azdine.eljattari@hotmail.com>
2021-05-13 03:33:11 +0200

    Added smtp server and will send confirmation email after appointment has been submitted

--- WebshopBouidi/Controllers/AppointmentController.cs
+++ WebshopBouidi/Controllers/AppointmentController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 using WebshopBouidi.BAL.Appointment;
 using WebshopBouidi.Models;
 
@@ -15,13 +18,28 @@ namespace WebshopBouidi.Controllers
 
         //POST: Appointment
         [HttpPost]
-        public ActionResult Create(AppointmentModel appointment)
+        public async Task<ActionResult> Create(AppointmentModel appointment) //DUBBLE CHECK
         {
             if (ModelState.IsValid)
             {
-                AppointmentBAL.CreateAppointment(appointment);
-                ViewBag.Msg = "Dankuwel voor het maken van een afspraak!";
-                return RedirectToAction("Index", "Appointment");
+                var body = $"<p>Beste {appointment.CustomerName}, </p><p>Uw afspraak voor {appointment.AppointmentDate.ToString("g")} is succesvol vastgesteld. Gelieve zeker een mondmasker mee te nemen en 15 minuten op voorhand aan te komen op locatie!</p><br/><p>Met vriendelijke groet,</p><br/><p>Kapper Bouidi</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress(appointment.Email));
+                message.From = new MailAddress("testmail.bouidi@gmail.com");
+                message.Subject = $"Afspraak bevestiging {appointment.AppointmentDate.ToString("g")}";
+                message.Body = string.Format(body);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential(System.Configuration.ConfigurationManager.AppSettings["FromEmail"], System.Configuration.ConfigurationManager.AppSettings["FromPassword"]);
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    AppointmentBAL.CreateAppointment(appointment);
+                    ViewBag.Msg = "Dankuwel voor het maken van een afspraak!";
+                    return RedirectToAction("Index", "Appointment");
+                }
             }
             return PartialView("Index", appointment);
         }
